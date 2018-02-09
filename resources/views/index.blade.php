5beb2b1ebdee0ab42f812ee2912926b9e7de7827
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2018-02-09 23:31:11 +0530

    UI fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,11 @@
       <p class="lead text-muted">A book is a gift you can open again and again.</p>
       <p>
         <a href="/catalog" class="btn btn-primary my-2">Browse our collection!</a>
-        <a href="/register" class="btn btn-secondary my-2">Sign Up</a>
+        @auth
+          <a href="/dashboard" class="btn btn-secondary my-2">Dashboard</a>
+        @else
+          <a href="/register" class="btn btn-secondary my-2">Sign Up</a>
+        @endauth
       </p>
   </section>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,7 +14,7 @@
     @include('include.messages')
     @yield('content')
   </main>
-  <footer class="container">
+  <footer class="container fixed-bottom">
     <p>&copy; 2018</p>
   </footer>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
