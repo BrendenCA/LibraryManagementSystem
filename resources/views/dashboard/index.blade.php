1f9494fc1568358cfbfdc72d97b52b6f20825163
Brenden Carvalho <BrendenCA@users.noreply.github.com>
2018-02-13 12:37:38 +0530

    Dashboard links to create

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,29 +4,16 @@
   <div class="row">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">Dashboard</div>
-
-        <div class="card-body">
-          @if (session('status'))
-            <div class="alert alert-success">
-              {{ session('status') }}
-            </div>
-          @endif
-
-          You are logged in!
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="card">
         <div class="card-header">Actions</div>
         <div class="card-body">
           <div class="btn-group-vertical" role="group">
             <a class="btn btn-link text-left" href="/role/edit">Change account type</a>
             <a class="btn btn-link text-left" href="/credits">Manage Credits</a>
             <a class="btn btn-link text-left" href="/library">Current borrows</a>
+            <a class="btn btn-link text-left" href="/catalog/create">Add catalog item</a>
+            <a class="btn btn-link text-left" href="/author/create">Add author</a>
+            <a class="btn btn-link text-left" href="/genre/create">Add genre</a>
           </div>
-
         </div>
       </div>
     </div>
